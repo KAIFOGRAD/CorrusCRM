e928963cf06a058c42a62064209d7b86b4e887a7
sharkioso <tetkin04@mail.ru>
2025-07-18 01:10:44 +0500

    теперь точно создал сущности для бд

--- crm-service/src/main/java/com/corrus/crm_service/Entity/Vehicle.java
+++ crm-service/src/main/java/com/corrus/crm_service/Entity/Vehicle.java
@@ -1,10 +1,39 @@
 package com.corrus.crm_service.Entity;
 
+import java.math.BigDecimal;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
+@AllArgsConstructor
+@Table(name = "vehicles")
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int totalCount;
+
+    private int availableCount;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
     
+    @Column(name = "price", precision = 14, scale = 2)
+    private BigDecimal price;
+
+    private String brand;
+
+    private String model;
+
+    private String producerCountry;
 }
